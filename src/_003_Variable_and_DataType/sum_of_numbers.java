d0f64ae917af92a0a22b28346663fb774956ff28
Your Name <kdhakad602@gmail.com>
2025-02-26 22:34:02 +0530

    Sum of two numbers

---
+++ src/_003_Variable_and_DataType/sum_of_numbers.java
@@ -0,0 +1,15 @@
+package _003_Variable_and_DataType;
+// Sum of two numbers
+public class sum_of_numbers {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 34;
+        int sum = a+b;
+        System.out.println(sum);
+//        System.out.println(a+b);
+    }
+}
+/*
+Output:
+46
+ */
