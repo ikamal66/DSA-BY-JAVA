0a038e95091edaf145681b8784448e3df2dc0f38
Your Name <kdhakad602@gmail.com>
2025-02-26 22:33:24 +0530

    Data Types in JAVA

---
+++ src/_003_Variable_and_DataType/dataTypes.java
@@ -0,0 +1,16 @@
+package _003_Variable_and_DataType;
+// Data Types in JAVA
+public class dataTypes {
+    public static void main(String[] args) {
+        byte b = 34;
+        System.out.println(b);
+        char c = 'p';
+        System.out.println(c);
+        boolean var = true;
+        float a1 = 1.0f;
+        int a2 = 13;
+        long a3 = 1000000000000000000L;
+        double a4 = 2.5;
+        short a5 = 20;
+    }
+}
