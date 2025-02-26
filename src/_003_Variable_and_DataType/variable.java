1be06ec22dc79129fc820434a39efe65a2233892
Your Name <kdhakad602@gmail.com>
2025-02-26 22:35:12 +0530

    variables

---
+++ src/_003_Variable_and_DataType/variable.java
@@ -0,0 +1,19 @@
+package _003_Variable_and_DataType;
+// variables is a container which stores a value
+public class variable {
+    public static void main(String[] args) {
+        int a = 14; // here,'a' is a variable
+        int b = 59;
+        System.out.println(a);
+         a = 254;
+        System.out.println(a);
+        a=b;
+        System.out.println(a);
+    }
+}
+/*
+Output:
+14
+254
+59
+ */
