cb515bbca2a47e513c275e197930056eac156d8d
Your Name <kdhakad602@gmail.com>
2025-02-26 19:11:23 +0530

    Print the pattern of star using of println

---
+++ src/_002_Print_Pattern/pattern.java
@@ -0,0 +1,10 @@
+package _002_Print_Pattern;
+// Print the pattern using of printf
+public class pattern {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
