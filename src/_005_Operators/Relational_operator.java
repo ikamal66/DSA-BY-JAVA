883918b27f3eafb9c0df08a3ae162cc4d4188477
Your Name <kdhakad602@gmail.com>
2025-02-27 13:53:55 +0530

    Relational (==, !=, <, >, >=, <= ) Operators

---
+++ src/_005_Operators/Relational_operator.java
@@ -0,0 +1,35 @@
+package _005_Operators;
+// Relational (==, !=, <, >, >=, <= ) Operators
+public class Relational_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Equality operator
+        System.out.println(a==b);
+
+        // Not equal operator
+        System.out.println(a!=b);
+
+        // Greater than operator
+        System.out.println(a>b);
+
+        // Less than operator
+        System.out.println(a<b);
+
+        // Greater than or equal to operator
+        System.out.println(a>=b);
+
+        // Less than or equal to operator
+        System.out.println(a<=b);
+    }
+}
+/*
+Output:
+false
+true
+true
+false
+true
+false
+ */
