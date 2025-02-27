01678c178f5453619fbe9f4bd364fed02821d905
Your Name <kdhakad602@gmail.com>
2025-02-27 13:54:20 +0530

    Unary (++a, a++, --a, --a) Operators

---
+++ src/_005_Operators/unary.java
@@ -0,0 +1,48 @@
+package _005_Operators;
+// Unary (++a, a++, --a, --a) Operators
+public class unary {
+    public static void main(String[] args) {
+        // pre-increment
+//        int a = 10;
+//        int b = ++a;
+//        System.out.println(a);
+//        System.out.println(b);
+        /*
+        Output:
+        11
+        11
+         */
+        // post-increment
+//        int a = 10;
+//        int b = a++;
+//        System.out.println(a);
+//        System.out.println(b);
+        /*
+        Output:
+        11
+        10
+         */
+
+        // pre-decrement
+//        int a = 10;
+//        int b = --a;
+//        System.out.println(a);
+//        System.out.println(b);
+        /*
+        Output:
+        9
+        9
+         */
+
+        // post-decrement
+//        int a = 10;
+//        int b = a--;
+//        System.out.println(a);
+//        System.out.println(b);
+        /*
+        Output:
+        9
+        10
+         */
+    }
+}
