add5723bd5343bc5624bd6087b120649ce1ba84b
Your Name <kdhakad602@gmail.com>
2025-02-27 13:52:35 +0530

    Assignment Operators +=, -=, *=, /=, %=,==

---
+++ src/_005_Operators/Assignment_Operator.java
@@ -0,0 +1,27 @@
+package _005_Operators;
+// Assignment Operators +=, -=, *=, /=, %=,==
+public class Assignment_Operator {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        // Addition assignment
+        x += y; // x = x + y
+        System.out.println(x); // 30
+
+        // Subtraction assignment
+        x -= y; // x = x - y
+        System.out.println(x); // 10
+
+        // Multiplication assignment
+        x *= y; // x = x * y
+        System.out.println(x); // 200
+
+        // Division assignment
+        x /= y; // x = x / y
+        System.out.println(x); // 10
+
+        // Remainder assignment
+        x %= y; // x = x % y
+        System.out.println(x); // 10
+    }
+}
