0dd280abe726e460231c71bf09141fca68ef09d4
Your Name <kdhakad602@gmail.com>
2025-02-27 13:39:11 +0530

    Arithmetic (+, - , *, /, %) Operations

---
+++ src/_005_Operators/Arithmetic_operator.java
@@ -0,0 +1,45 @@
+package _005_Operators;
+// Arithmetic (+, - , *, /, %) Operations
+public class Arithmetic_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Addition: " + sum);
+
+        // Subtraction
+        int difference = a - b;
+        System.out.println("Subtraction: " + difference);
+
+        // Multiplication
+        int product = a * b;
+        System.out.println("Multiplication: " + product);
+
+        // Division
+        int quotient = a / b;
+        System.out.println("Division: " + quotient);
+
+        // Modulus (remainder)
+        int remainder = a % b;
+        System.out.println("Modulus: " + remainder);
+
+        // Increment (a++)
+//        a++;
+//        System.out.println("Incremented value of a: " + a);
+
+        // Decrement (a--)
+//        a--;
+//        System.out.println("Decrement value of a: " + a);
+    }
+}
+/*
+Output:
+Addition: 15
+Subtraction: 5
+Multiplication: 50
+Division: 2
+Modulus: 0
+Incremented value of a: 11
+ */
