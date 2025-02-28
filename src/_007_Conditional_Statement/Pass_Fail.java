9b4cdd25efa80733c0cf5392cb3754331cb09744
Your Name <kdhakad602@gmail.com>
2025-02-28 10:41:05 +0530

    Using of Ternary Operator

---
+++ src/_007_Conditional_Statement/Pass_Fail.java
@@ -0,0 +1,13 @@
+package _007_Conditional_Statement;
+// Using of Ternary Operator
+public class Pass_Fail {
+    public static void main(String[] args) {
+        int number = 76;
+        String result = (number >= 60)? "Pass" : "Fail";
+        System.out.println(result);
+    }
+}
+/*
+Output:
+Pass
+ */
