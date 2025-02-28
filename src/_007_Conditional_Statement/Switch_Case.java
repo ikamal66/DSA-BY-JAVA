3ebd9df201d56babb7e42ca9aaad6072d5765514
Your Name <kdhakad602@gmail.com>
2025-02-28 10:41:29 +0530

    Switch Case

---
+++ src/_007_Conditional_Statement/Switch_Case.java
@@ -0,0 +1,24 @@
+package _007_Conditional_Statement;
+// Switch Case
+public class Switch_Case {
+    public static void main(String[] args) {
+        int number = 3;
+        switch (number) {
+            case 1:
+                System.out.println("Burger");
+                break;
+            case 2:
+                System.out.println("Momos");
+                break;
+            case 3:
+                System.out.println("Daaru");
+                break;
+            default:
+                System.out.println("wake up");
+        }
+    }
+}
+/*
+Output:
+Daaru
+ */
