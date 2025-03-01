9409e50b4978622dd8d0f1da5f210d9c83c2326b
Your Name <kdhakad602@gmail.com>
2025-03-01 18:18:28 +0530

    Printing Hello World Using the While Loop

---
+++ src/_009_Loops/_02while_loop.java
@@ -0,0 +1,67 @@
+package _009_Loops;
+// Printing Hello World Using the While Loop
+public class _02while_loop {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<50){
+            System.out.println("Hello World");
+            i++;
+        }
+    }
+}
+/*
+output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+
+Process finished with exit code 0
+
+ */
