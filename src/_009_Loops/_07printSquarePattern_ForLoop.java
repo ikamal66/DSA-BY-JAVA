df5b852e0975321e242607ec33c0a9de8e99d843
Your Name <kdhakad602@gmail.com>
2025-03-01 18:20:00 +0530

    Print the Square pattern

---
+++ src/_009_Loops/_07printSquarePattern_ForLoop.java
@@ -0,0 +1,16 @@
+package _009_Loops;
+// Print the Square pattern
+public class _07printSquarePattern_ForLoop {
+    public static void main(String[] args) {
+        for(int line=1; line<=4; line++){
+            System.out.println("**** ");
+        }
+
+        // Using a while loop
+//        int i=1;
+//        while(i<=4){
+//            System.out.println("**** ");
+//            i++;
+//        }
+    }
+}
