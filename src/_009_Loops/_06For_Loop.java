2d8e39ee556d0685da350549024b39a3dc20009b
Your Name <kdhakad602@gmail.com>
2025-03-01 18:19:47 +0530

    using of For Loop

---
+++ src/_009_Loops/_06For_Loop.java
@@ -0,0 +1,17 @@
+package _009_Loops;
+// using of For Loop
+public class _06For_Loop {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
+/*
+Output:
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+ */
