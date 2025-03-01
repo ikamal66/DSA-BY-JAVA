2ba58070bc2f8dfc504b7ee576fda031114bda8b
Your Name <kdhakad602@gmail.com>
2025-03-01 18:19:04 +0530

    print 1 to 50 numbers using of While Loops in Horizontal

---
+++ src/_009_Loops/_04printNumber_whileLoop.java
@@ -0,0 +1,17 @@
+package _009_Loops;
+// print 1 to 50 numbers using of While Loops in Horizontal
+public class _04printNumber_whileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 50) {
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println();
+    }
+}
+/*
+Output:
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+
+ */
