b77b9022b7811290b44302defafd629d419f0fe1
Your Name <kdhakad602@gmail.com>
2025-03-01 18:20:31 +0530

    Do-while Loop

---
+++ src/_009_Loops/_09Do_While_Loop.java
@@ -0,0 +1,24 @@
+package _009_Loops;
+// Do-while Loop
+public class _09Do_While_Loop {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("Hello World");
+            i++;
+        }while(i<=10);
+    }
+}
+/*
+output:
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+ */
