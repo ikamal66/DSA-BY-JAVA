c734633b167d74fb4b2efda8649b77d28b4ef89e
Your Name <kdhakad602@gmail.com>
2025-03-01 18:27:38 +0530

    How many times 'Hello' is printed?

---
+++ src/_010_QuestionsOnLoops/_01Question.java
@@ -0,0 +1,20 @@
+package _010_QuestionsOnLoops;
+/*
+ How many times 'Hello' is printed?
+public class Solution {
+  public static void main (String[] args) {
+    for(int i=0; i<5; i++) {
+      System.out.println("Hello");
+      i+=2;
+    }
+  }
+}
+ */
+public class _01Question {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++) {
+            System.out.println("Hello");
+            i+=2;
+        }
+    }
+}
