4b54498efd44675dbdd2ccb2d56fcfa69b3f6be1
Your Name <kdhakad602@gmail.com>
2025-03-02 23:31:02 +0530

    Inverted Star Pattern

---
+++ src/_011_Pattern_Problem_Part_1/_02Inverted_Star.java
@@ -0,0 +1,28 @@
+package _011_Pattern_Problem_Part_1;
+// Inverted Star Pattern
+/*
+
+     * * * *
+     * * *
+     * *
+     *
+
+ */
+public class _02Inverted_Star {
+    public static void main(String[] args) {
+        int n=4;
+//        for(int i=1; i<=n; i++){
+//            for(int j=1; j<=n-i+1; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+
+             // Alternate Method
+            for(int i=1; i<=n; i++){
+                for(int j=n; j>=i; j--){
+                    System.out.print("* ");
+                }
+                System.out.println();
+        }
+    }
+}
