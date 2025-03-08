6e91b75bb1f70af360d9b5d23bd3dc470a72229c
Your Name <kdhakad602@gmail.com>
2025-03-08 21:22:43 +0530

    Function call

---
+++ src/_012_Functions_and_Methods/_01_FunctionCall.java
@@ -0,0 +1,23 @@
+package _012_Functions_and_Methods;
+// Function call
+public class _01_FunctionCall {
+    public static void printStatements(){
+        System.out.println("I am kamal");
+        System.out.println("I have crush on you");
+        System.out.println("I am in love");
+        System.out.println("I am a college student");
+        System.out.println("I am in 6th semester");
+        return;
+    }
+    public static void main(String[]args){
+        printStatements(); // calling the function
+    }
+}
+/*
+Output:
+I am kamal
+I have crush on you
+I am in love
+I am a college student
+I am in 6th semester
+ */
