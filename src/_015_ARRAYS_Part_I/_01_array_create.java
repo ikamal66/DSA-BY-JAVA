1dc7e85d8ca396bdd0d7ee3f86916c8cd1b523a3
Your Name <kdhakad602@gmail.com>
2025-04-08 19:57:19 +0530

    Create an array

---
+++ src/_015_ARRAYS_Part_I/_01_array_create.java
@@ -0,0 +1,16 @@
+package _015_ARRAYS_Part_I;
+// Create an array
+public class _01_array_create {
+    public static void main(String[] args) {
+        int marks[] = new int[50];  // Declaration + memory allocation
+        int[] mark = new int [3];
+        // int marks[]  || int[] mark  these are same
+
+        int numbers[] = {1,2,3}; // Declare + initialization
+
+        int result[]; //Declare
+        result = new int[6]; // Memory allocation
+
+        String fruits[] = {"apple", "orange"};
+    }
+}
