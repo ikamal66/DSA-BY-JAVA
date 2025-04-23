3961ecd64c42c44bcea8e5ed2e15693329564c2c
Your Name <kdhakad602@gmail.com>
2025-04-23 21:41:34 +0530

    Print Sum of even and Odd

---
+++ src/_010_QuestionsOnLoops/_02_Question.java
@@ -0,0 +1,32 @@
+package _010_QuestionsOnLoops;
+import java.util.Scanner;
+
+/*
+Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+ */
+public class _02_Question {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        int choice;
+        int evenSum = 0;
+        int oddSum = 0;
+
+        do{
+            System.out.println("Enter the number");
+            number = sc.nextInt();
+
+            if(number%2 == 0){
+                evenSum += number;
+            }
+            else{
+                oddSum += number;
+            }
+            System.out.println("Do you want to continue? Press 1 for yes or 0 for no");
+            choice = sc.nextInt();
+        } while(choice == 1);
+
+        System.out.println("Sum of even numbers: "+evenSum);
+        System.out.println("Sum of odd numbers: "+oddSum);
+    }
+}
