4d36e19a8b31495bfc997985bcffe11ce4705ed5
Your Name <kdhakad602@gmail.com>
2025-04-24 23:59:55 +0530

    How to create a String

---
+++ src/_019_Strings/_01_Strings_introduction.java
@@ -0,0 +1,10 @@
+package _019_Strings;
+
+public class _01_Strings_introduction {
+    public static void main(String[] args) {
+        char[] arr = {'a', 'b', 'c', 'd'};
+        String str = "abcd"; // Memory Location: String Pool (Optimized Memory Usage)
+        String str1 = new String("xyz");// Memory Location: Heap Memory (Always Creates New Object)
+        // Strings are IMMUTABLE
+    }
+}
