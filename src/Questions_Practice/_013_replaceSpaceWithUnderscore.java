e28afc8eaf2b87c0756af055540769c50de962f0
Your Name <kdhakad602@gmail.com>
2025-04-25 13:55:26 +0530

    Replace spaces with underscores

---
+++ src/Questions_Practice/_013_replaceSpaceWithUnderscore.java
@@ -0,0 +1,14 @@
+package Questions_Practice;
+/*
+    Problem :Write a java program to replace spaces with underscores.
+ */
+public class _013_replaceSpaceWithUnderscore {
+    public static void main(String[] args) {
+        String str = "Her name is  Shefali";
+        System.out.println(str.replace(" ","_"));
+    }
+}
+/*
+Output:
+Her_name_is__Shefali
+ */
