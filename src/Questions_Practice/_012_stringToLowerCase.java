d9866ba2b00ec3bd07b0a55998f6d335dbd26f19
Your Name <kdhakad602@gmail.com>
2025-04-25 13:55:02 +0530

    convert a String to a lowerCase

---
+++ src/Questions_Practice/_012_stringToLowerCase.java
@@ -0,0 +1,15 @@
+package Questions_Practice;
+/*
+    Problem 1 :Write a java program to convert a String to a lowerCase.
+ */
+public class _012_stringToLowerCase {
+    public static void main(String[] args) {
+        String str = "My name is Kamal SINGH dhAKad";
+        System.out.println(str.toLowerCase());
+    }
+}
+/*
+Output:
+my name is kamal singh dhakad
+ */
+
