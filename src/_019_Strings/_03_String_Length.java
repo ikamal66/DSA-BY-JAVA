8a92243cbb79da8ae103332df19fe1c2e77ea8bd
Your Name <kdhakad602@gmail.com>
2025-04-25 23:51:25 +0530

    Finding the String Length

---
+++ src/_019_Strings/_03_String_Length.java
@@ -0,0 +1,13 @@
+package _019_Strings;
+// Finding the String Length
+public class _03_String_Length {
+    public static void main(String[] args) {
+        String fullName = "Kamal Singh";
+        System.out.println(fullName.length());
+        // here, length() is a function. That, why we use of () in front of length
+    }
+}
+/*
+Output:
+11
+ */
