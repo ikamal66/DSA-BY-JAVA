0e42e2807ff676d318ba7028cb5f376b82e21f98
Your Name <kdhakad602@gmail.com>
2025-04-25 23:55:53 +0530

    Question on letters

---
+++ src/_019_Strings/_12_LettersTo_Uppercase.java
@@ -0,0 +1,36 @@
+package _019_Strings;
+/*
+Question: For a given String convert each the first letter of each word to uppercase.
+            "hi,i am kamal"
+ */
+
+// Time Complexity = O(n)
+public class _12_LettersTo_Uppercase {
+    public static String toUpperCase(String str){
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(i) == ' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "hi, i am kamal";
+        System.out.println(toUpperCase(str));
+    }
+}
+/*
+Output:
+Hi, I Am Kamal
+ */
