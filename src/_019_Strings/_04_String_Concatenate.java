03068940b7fba27dc8c3638de1eead6f4adf2313
Your Name <kdhakad602@gmail.com>
2025-04-25 23:51:40 +0530

    Concatenation in String

---
+++ src/_019_Strings/_04_String_Concatenate.java
@@ -0,0 +1,15 @@
+package _019_Strings;
+// Concatenation in String
+public class _04_String_Concatenate {
+    public static void main(String[] args) {
+        // Concatenate means, 2 cheezo ko add karna
+        String firstName = "Kamal";
+        String middleName = "Singh";
+        String fullName = firstName + " " + middleName; // this is called concatenation
+        System.out.println(fullName);
+    }
+}
+/*
+Output:
+Kamal Singh
+ */
