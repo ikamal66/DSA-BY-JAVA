ffe2c0afd7b27d10dfd12b97504835839e81b4f2
Your Name <kdhakad602@gmail.com>
2025-04-30 18:16:46 +0530

    Xor ^ in Binary

---
+++ src/_021_Bit_Manipulation/_03_Binary_XOR.java
@@ -0,0 +1,19 @@
+package _021_Bit_Manipulation;
+// Xor ^ in Binary
+/*
+0 ^ 0 - 0
+0 ^ 1 - 1
+1 ^ 0 - 1
+1 ^ 1 - 0
+ */
+public class _03_Binary_XOR {
+    public static void main(String[] args) {
+        System.out.println(5 ^ 6);
+        System.out.println(9 ^ 2);
+    }
+}
+/*
+Output:
+3
+11
+ */
