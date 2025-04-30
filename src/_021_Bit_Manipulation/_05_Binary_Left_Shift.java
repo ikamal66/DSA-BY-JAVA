856c5ad2ed5183ea1663f4fb0241be49d8954f27
Your Name <kdhakad602@gmail.com>
2025-04-30 18:17:14 +0530

    Left shift a << b

---
+++ src/_021_Bit_Manipulation/_05_Binary_Left_Shift.java
@@ -0,0 +1,17 @@
+package _021_Bit_Manipulation;
+// Left shift a << b
+/*
+Formula -- a << b
+use  --    a * 2^b
+ */
+public class _05_Binary_Left_Shift {
+    public static void main(String[] args) {
+        System.out.println(5 << 2);
+        System.out.println(10 << 2);
+    }
+}
+/*
+Output:
+20
+40
+ */
