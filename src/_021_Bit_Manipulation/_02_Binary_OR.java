da50d0d61df6efebb44f4c8dd4c14d6418c2ff2a
Your Name <kdhakad602@gmail.com>
2025-04-30 18:16:28 +0530

    OR | in Binary

---
+++ src/_021_Bit_Manipulation/_02_Binary_OR.java
@@ -0,0 +1,14 @@
+package _021_Bit_Manipulation;
+// OR | in Binary
+public class _02_Binary_OR {
+    public static void main(String[] args) {
+        System.out.println(5 | 6);
+        System.out.println(9 | 2);
+
+    }
+}
+/*
+Output:
+7
+11
+ */
