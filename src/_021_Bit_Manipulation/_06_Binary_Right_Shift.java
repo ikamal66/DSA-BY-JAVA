d0d8f5a630a4f250d3adde19caa8ed6709f5ad03
Your Name <kdhakad602@gmail.com>
2025-04-30 18:17:25 +0530

    Right Shift a >> b

---
+++ src/_021_Bit_Manipulation/_06_Binary_Right_Shift.java
@@ -0,0 +1,15 @@
+package _021_Bit_Manipulation;
+// Right Shift a >> b
+/*
+Formula -- a >> b
+use  --    a/2^b
+ */
+public class _06_Binary_Right_Shift {
+    public static void main(String[] args) {
+        System.out.println(6 >> 1);
+    }
+}
+/*
+Output:
+3
+ */
