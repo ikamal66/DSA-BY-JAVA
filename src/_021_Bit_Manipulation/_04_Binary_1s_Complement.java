1b8b00e374da21e320736bcae0d1e5566c5cbb9b
Your Name <kdhakad602@gmail.com>
2025-04-30 18:17:00 +0530

    1's Complement

---
+++ src/_021_Bit_Manipulation/_04_Binary_1s_Complement.java
@@ -0,0 +1,13 @@
+package _021_Bit_Manipulation;
+// 1's Complement
+public class _04_Binary_1s_Complement {
+    public static void main(String[] args) {
+        System.out.println(~5);
+        System.out.println(~7);
+    }
+}
+/*
+Output:
+-6
+-8
+ */
