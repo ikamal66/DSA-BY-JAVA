067b171555a758cadc69bc11fda648d00b43c0a2
Your Name <kdhakad602@gmail.com>
2025-04-30 18:16:16 +0530

    AND & in Binary

---
+++ src/_021_Bit_Manipulation/_01_Binary_AND.java
@@ -0,0 +1,13 @@
+package _021_Bit_Manipulation;
+// AND & in Binary
+public class _01_Binary_AND {
+    public static void main(String[] args) {
+        System.out.println(5 & 6);
+        System.out.println(9 & 2);
+    }
+}
+/*
+Output:
+4
+0
+ */
