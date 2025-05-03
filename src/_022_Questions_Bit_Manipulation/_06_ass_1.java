d985329199c1de7232a78d18a387cdacd8ac7bbc
Your Name <kdhakad602@gmail.com>
2025-05-03 23:16:33 +0530

    What is the value of x^x for any valueof x?

---
+++ src/_022_Questions_Bit_Manipulation/_06_ass_1.java
@@ -0,0 +1,11 @@
+package _022_Questions_Bit_Manipulation;
+// What is the value of x^x for any valueof x?
+public class _06_ass_1 {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x ^ x);
+    }
+}
+/*
+0
+ */
