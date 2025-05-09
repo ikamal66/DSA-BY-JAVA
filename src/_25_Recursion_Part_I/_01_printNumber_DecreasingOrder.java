ecd7df6dac95811058676f0fd77954d56da4b1fc
Your Name <kdhakad602@gmail.com>
2025-05-09 22:56:07 +0530

    Print numbers in decreasing order

---
+++ src/_25_Recursion_Part_I/_01_printNumber_DecreasingOrder.java
@@ -0,0 +1,20 @@
+package _25_Recursion_Part_I;
+//Print numbers in decreasing order
+public class _01_printNumber_DecreasingOrder {
+    public static void printDec(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+    public static void main(String[] args) {
+        int n =10;
+        printDec(10);
+    }
+}
+/*
+Output:
+10 9 8 7 6 5 4 3 2 1
+ */
