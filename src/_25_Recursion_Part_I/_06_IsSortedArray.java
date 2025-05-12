c26cb5041af8fc4c0de5480cd12f817e2b1829d5
Your Name <kdhakad602@gmail.com>
2025-05-12 21:17:20 +0530

    Find Array is Sorted or not

---
+++ src/_25_Recursion_Part_I/_06_IsSortedArray.java
@@ -0,0 +1,27 @@
+package _25_Recursion_Part_I;
+// Find Array is Sorted or not
+
+// Time & Space Complexity = O(n)
+public class _06_IsSortedArray {
+    public static boolean isSorted(int[] arr, int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+
+        return isSorted(arr, i+1);
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1,2,3,4,5};
+        int[] arr1 = { 1,2,7,4,5};
+        System.out.println(isSorted(arr,0));
+        System.out.println(isSorted(arr1,0));
+    }
+}
+/*
+Output:
+true
+false
+ */
