77fc35948dd849cfcc60f66bf59dfe73526d4838
Your Name <kdhakad602@gmail.com>
2025-05-16 20:53:54 +0530

    Find the Last Occurrence of an element in an array

--- src/_25_Recursion_Part_I/_08_Last_Occurrence.java
+++ src/_25_Recursion_Part_I/_08_Last_Occurrence.java
@@ -1,23 +1,41 @@
 package _25_Recursion_Part_I;
-// Find the Last Occurence of an element in an array
+// Find the Last Occurrence of an element in an array
 
 public class _08_Last_Occurrence {
-    public static int lastOccurence(int[] arr, int key, int i){
+    public static int lastOccurrence(int[] arr, int key, int i){
         if( i == arr.length){
             return -1;
         }
-        int isFound = lastOccurence(arr,key,i+1);
+        int isFound = lastOccurrence(arr,key,i+1);
         if(isFound == -1 && arr[i] == key){
             return i;
         }
         return isFound;
     }
+
+    //Give same answer as above func
+    public static int lastOcc(int[] arr, int i, int key){
+        //base condition
+        if(i < 0){
+            return -1;
+        }
+
+        //kaam
+        if(arr[i] == key){
+            return i;
+        }
+
+        return lastOcc(arr,i-1,key);
+    }
+
     public static void main(String[] args) {
         int[] arr = {5,5,5,5};
-        System.out.println(lastOccurence(arr,5,0));
+        System.out.println(lastOccurrence(arr,5,0));
+        System.out.println(lastOcc(arr,arr.length-1,5));
     }
 }
 /*
 Output:
+3
 3
  */
